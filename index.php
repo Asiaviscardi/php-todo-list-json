508d7185fd09a71816a47af058dd7dad9519c878
Asiaviscardi <asia.viscardii@gmail.com>
2023-10-30 14:05:50 +0100

    inserimento struttura

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Todo list</title>
+
+</head>
+
+<body>
+
+    <h1>Todo List</h1>
+
+    <div class = "container">
+        <div>
+            tasks
+        </div>
+    </div>
+
+
+    
+</body>
+
+</html>
